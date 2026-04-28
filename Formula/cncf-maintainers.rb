d6070e45f2346d109390b7363e28cac1f53fb792
Ihor Dvoretskyi <ihor@linux.com>
2026-04-29 01:05:19 +0300

    chore(cncf-maintainers): bump formula to v0.2.0 (#5)
    Updates the formula to track the newly published cncf-maintainers
    v0.2.0 release. All four sha256s verified against the upstream
    checksums.txt.
    
    Also eliminates hard-coded version strings: the formula test block
    now uses 'version.to_s' and both e2e workflow jobs now read the
    expected version dynamically from the formula file, so future
    formula bumps require no changes to the workflow.
    
    Signed-off-by: Ihor Dvoretskyi <ihor@linux.com>

--- Formula/cncf-maintainers.rb
+++ Formula/cncf-maintainers.rb
@@ -4,28 +4,28 @@
 class CncfMaintainers < Formula
   desc "CLI to validate GitHub usernames against the CNCF maintainers list"
   homepage "https://github.com/idvoretskyi/cncf-github-maintainers"
-  version "0.1.0"
+  version "0.2.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.1.0/cncf-maintainers_darwin_arm64.tar.gz"
-      sha256 "bce0db61fbedeb7cfd2b3a9d1fa1eb1853b107264221f8d122fc0161e57237fd"
+      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.2.0/cncf-maintainers_darwin_arm64.tar.gz"
+      sha256 "070a7fd67f207aa3db9ccced2a258a7a50aec3c5158d862481a3cbf61de6587d"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.1.0/cncf-maintainers_darwin_amd64.tar.gz"
-      sha256 "5c4fa6b5b6408a3101e450619e1c7098eda2683f30311cc52caf455f49392d60"
+      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.2.0/cncf-maintainers_darwin_amd64.tar.gz"
+      sha256 "90dd0d621388c48629152b15400704a9b0924e40dd62259c34507ccdf0ff7463"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.1.0/cncf-maintainers_linux_arm64.tar.gz"
-      sha256 "15d7cc2ccd75e36d2be0cfe50a679985d818f9ae08f5831ea5f15a637ab1fd8d"
+      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.2.0/cncf-maintainers_linux_arm64.tar.gz"
+      sha256 "f9dc7d2ad59cfaafecbbf021b918aa4e83c4aef2491b21397cdc6dbac977f999"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.1.0/cncf-maintainers_linux_amd64.tar.gz"
-      sha256 "928fdf68f2f95083f2e3b7464319dcca410bca8e5f83ef355990a028949f4bca"
+      url "https://github.com/idvoretskyi/cncf-github-maintainers/releases/download/v0.2.0/cncf-maintainers_linux_amd64.tar.gz"
+      sha256 "2c66580b18af67e0906995fc363a86c62daafd3c09df4d57fdf86f8d2d205757"
     end
   end
 
@@ -34,6 +34,6 @@ class CncfMaintainers < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/cncf-maintainers --version")
+    assert_match version.to_s, shell_output("#{bin}/cncf-maintainers --version")
   end
 end
